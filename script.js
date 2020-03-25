1370d24c8aa9ca5c599618c8fc9ed6126e19842f
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-25 21:24:03 +0300

    feat: start singolo responsive

--- script.js
+++ script.js
@@ -7,26 +7,26 @@ MENU.addEventListener("click", event => {
   event.target.classList.add("nav_active");
 });
 
-document.addEventListener('scroll', onScroll);
+document.addEventListener("scroll", onScroll);
 function onScroll(event) {
   const currentPosition = window.scrollY;
-  const header = document.querySelector('header');
-  const sections = document.querySelectorAll('body > *');
-  const navigation = document.querySelectorAll('#menu li a');
+  const header = document.querySelector("header");
+  const sections = document.querySelectorAll("body > *");
+  const navigation = document.querySelectorAll("#menu li a");
 
   sections.forEach(element => {
-    if(element.hasAttribute('id')){
+    if (element.hasAttribute("id")) {
       if (
         element.offsetTop - header.clientHeight <= currentPosition &&
         element.offsetTop + element.clientHeight > currentPosition
-      ){
+      ) {
         navigation.forEach(link => {
-          link.classList.remove('nav_active');
-          if(
-            element.getAttribute('id') ===
-            link.getAttribute('href').substring(1)
-          ){
-            link.classList.add('nav_active');
+          link.classList.remove("nav_active");
+          if (
+            element.getAttribute("id") ===
+            link.getAttribute("href").substring(1)
+          ) {
+            link.classList.add("nav_active");
           }
         });
       }
@@ -36,59 +36,61 @@ function onScroll(event) {
 
 //! Slider
 
-let sliders = document.querySelectorAll('.sliders');
+let sliders = document.querySelectorAll(".sliders");
 let currentSliders = 0;
 let isEnable = true;
 
-function changeActiveSlider(n){
+function changeActiveSlider(n) {
   currentSliders = (n + sliders.length) % sliders.length;
 }
 
-function hideSlider(direction){
+function hideSlider(direction) {
   isEnable = false;
   sliders[currentSliders].classList.add(direction);
-  sliders[currentSliders].addEventListener('animationend', function(){
-    this.classList.remove('active', direction);
+  sliders[currentSliders].addEventListener("animationend", function() {
+    this.classList.remove("active", direction);
   });
 }
 
 function showSlider(direction) {
-  sliders[currentSliders].classList.add('next', direction);
-  sliders[currentSliders].addEventListener('animationend', function(){
-    this.classList.remove('next', direction);
-    this.classList.add('active');
+  sliders[currentSliders].classList.add("next", direction);
+  sliders[currentSliders].addEventListener("animationend", function() {
+    this.classList.remove("next", direction);
+    this.classList.add("active");
     isEnable = true;
   });
+
+  if (currentSliders == 1) {
+    document.querySelector("body > .carousel").style.backgroundColor =
+      "#648BF0";
+    document.querySelector("body > .carousel").style.borderColor = "#6f92ef";
+  } else {
+    document.querySelector("body > .carousel").style.backgroundColor =
+      "#f06c64";
+    document.querySelector("body > .carousel").style.borderColor = "#ea676b";
+  }
 }
 
-if(currentSliders === 1){
-  document.querySelector('body > main').style.backgroundColor = '#648BF0';
-  document.querySelector('body > main').style.borderColor = '#6f92ef';
-} else{
-  document.querySelector('body > main').style.backgroundColor = '#f06c64';
-  document.querySelector('body > main').style.borderColor = '#ea676b';
-}
-
-function previousSlider(n){
-  hideSlider('to-right');
+function previousSlider(n) {
+  hideSlider("to-right");
   changeActiveSlider(n - 1);
   showSlider("from-left");
 }
 
-function nextSlider(n){
-  hideSlider('to-left');
+function nextSlider(n) {
+  hideSlider("to-left");
   changeActiveSlider(n + 1);
-  showSlider('from-right');
+  showSlider("from-right");
 }
 
-document.querySelector('.arrows .left').addEventListener('click', function(){
-  if(isEnable){
+document.querySelector(".arrows .left").addEventListener("click", function() {
+  if (isEnable) {
     previousSlider(currentSliders);
   }
 });
 
-document.querySelector('.arrows .right').addEventListener('click', function(){
-  if(isEnable){
+document.querySelector(".arrows .right").addEventListener("click", function() {
+  if (isEnable) {
     nextSlider(currentSliders);
   }
 });
@@ -169,4 +171,3 @@ closeButton.addEventListener("click", () => {
   subject.value = "";
   formBlock.reset();
 });
-
